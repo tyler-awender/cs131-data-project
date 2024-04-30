b3400696eeab59c1e6e324443566b743523c7e3d
tsyed171 <tsyedew@gmail.com>
2024-04-30 16:49:47 -0700

    Updating TS awk script

---
+++ TS-Analysis/ts-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#create csv with following data: Artist,Track,Danceability,Valence
+awk -F ',' 'BEGIN{OFS=","} {print $2,$3,$6,$14}' New_Spotify_Youtube.csv > dance-val.csv
+
+#create csv with following data: Artist,Track,Tempo,Stream
+awk -F ',' 'BEGIN{OFS=","} {print $2,$3,$15,$22}' New_Spotify_Youtube.csv > tempo-stream.csv
