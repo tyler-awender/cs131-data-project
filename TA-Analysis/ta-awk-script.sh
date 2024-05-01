12d22276bd020a345541a87d7e71141b1ea7c0b4
tyler-awender <teelar101@gmail.com>
2024-04-30 23:09:12 -0700

    Changed awk script to include duration

--- TA-Analysis/ta-awk-script.sh
+++ TA-Analysis/ta-awk-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#csv with streams,views,likes,comments
-awk -F'>' 'BEGIN{ OFS=">" } { print $23, $19, $20, $21 }' ../Preprocessing+Dataset/data.csv > streams-views-likes-comments.csv
+#csv with streams,views,likes,comments,duration
+awk -F'>' 'BEGIN{ OFS=">" } { print $23, $19, $20, $21, $16 }' ../Preprocessing+Dataset/data.csv > streams-views-likes-comments-duration.csv
 
