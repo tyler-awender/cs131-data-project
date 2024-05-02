1567772b9dfd0bf8f884ada8239326183df6af33
Fariha Ahmed <102993036+fahmed0710@users.noreply.github.com>
2024-05-01 22:56:05 -0700

    Update create_csvs.sh

--- FA - Analysis/create_csvs.sh
+++ FA - Analysis/create_csvs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Creates csv with columns Track>Album_name>Album_type>Views>Likes>Comments>Stream
-awk -F'>' 'BEGIN { OFS=">"} { print $3, $4, $5, $17, $19, $20, $21, $23 }' data.csv > album_type_inferences.csv 
+awk -F'^' 'BEGIN { OFS="^"} { print $3, $4, $5, $17, $19, $20, $21, $23 }' New_Spotify_Youtube.csv > album_type_inferences.csv 
 
-# Creates csv with columns Track>Key>Views>Likes>Stream
-awk -F'>' 'BEGIN { OFS=">"} { print $3, $8, $19, $20, $23 }' data.csv > key_popularity_inferences.csv
+# Creates csv with columns Danceability>Energy>Key>Loudness>Speechiness>Acousticness>Instrumentalness>Liveness>Valence>Tempo>Duration_ms>Views>Likes>Comments>Stream
+awk -F'^' 'BEGIN { OFS="^"} { print $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $19, $20, $21, $23 }' New_Spotify_Youtube.csv > metrics_performance_inferences.csv
+
 
