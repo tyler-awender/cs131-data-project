3936318473f80118301631040d34ec4cb5c8982a
Fariha Ahmed <102993036+fahmed0710@users.noreply.github.com>
2024-05-01 22:43:55 -0700

    Update album_type_inferences.py

--- FA - Analysis/album_type_inferences.py
+++ FA - Analysis/album_type_inferences.py
@@ -7,17 +7,7 @@ filtered_albums = df.groupby('Album')['Album_type'].nunique()
 filtered_albums = filtered_albums[filtered_albums == 2].index.tolist()
 df2 = df[df['Album'].isin(filtered_albums)]
 
-# 1. Popularity of tracks -- analyzing the correlation between Youtube views and Spotify streams
-
-youtube_views = df['Views']
-spotify_streams = df['Stream']
-correlation = youtube_views.corr(spotify_streams)
-print("Correlation between YouTube views and Spotify streams: ", correlation)
-
-
-# 2. Album vs Single performance: compare the avg views/streams of tracks categorized as "album" vs "single"
-
-print('\n')
+# 1. Album vs Single performance: compare the avg views/streams of tracks categorized as "album" vs "single"
 
 album_views = df[df['Album_type'] == 'album']['Views']
 single_views = df[df['Album_type'] == 'single']['Views']
@@ -33,38 +23,7 @@ print("Avg views for album tracks: ", avg_album_views)
 print("Avg views for single tracks: ", avg_single_views)
 print("Avg streams for album tracks: ", avg_album_streams)
 print("Avg streams for single tracks: ", avg_single_streams)
-
-
-#3. Compare performance within the same album based on album type 
-
-print('\n')
-albums = df2['Album'].unique()
-# for album in albums:
-#     album_data = df2[df2['Album'] == album]
-#     album_single_views = album_data[album_data['Album_type'] == 'single']['Views']
-#     album_album_views = album_data[album_data['Album_type'] == 'album']['Views']
-#     album_single_streams = album_data[album_data['Album_type'] == 'single']['Stream']
-#     album_album_streams = album_data[album_data['Album_type'] == 'album']['Stream']
-
-#     print(f"Album: {album}")
-#     print("Average views for single tracks:", album_single_views.mean())
-#     print("Average views for album tracks:", album_album_views.mean())
-#     print("Average streams for single tracks:", album_single_streams.mean())
-#     print("Average streams for album tracks:", album_album_streams.mean())
-#     print()
     
-
-album_single_avg_views = []
-album_album_avg_views = []
-album_single_avg_streams = []
-album_album_avg_streams = []
-for album in albums:
-    album_data = df2[df2['Album'] == album]
-    album_single_avg_views.append(album_data[album_data['Album_type'] == 'single']['Views'].mean())
-    album_album_avg_views.append(album_data[album_data['Album_type'] == 'album']['Views'].mean())
-    album_single_avg_streams.append(album_data[album_data['Album_type'] == 'single']['Stream'].mean())
-    album_album_avg_streams.append(album_data[album_data['Album_type'] == 'album']['Stream'].mean())
-
 # Data Visualization
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 
